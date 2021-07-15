e8c197fb8a054799779afd332cffdc49ca2bd355
allan-xie <68440183@qq.com>
2021-07-15 17:42:59 +0800

    20210715 update

--- mysite/mysite/blog/views.py
+++ mysite/mysite/blog/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 
 # Create your views here.
+from taggit.models import Tag
 from django.core.mail import send_mail
 from .forms import EmailPostForm,CommentForm
 from django.views.generic import ListView
@@ -10,8 +11,14 @@ from django.core.paginator import Paginator,EmptyPage,\
 								  PageNotAnInteger
 
 
-def post_list(request):
+def post_list(request,tag_slug=None):
 	object_list = Post.published.all()
+	tag = None
+
+	if tag_slug:
+		tag = get_object_or_404(Tag,slug=tag_slug)
+		object_list = object_list.filter(tags__in=[tag])
+
 	paginator = Paginator(object_list, 3)
 	page = request.GET.get('page')
 	try:
@@ -25,7 +32,8 @@ def post_list(request):
 	return render(request,
 				'blog/post/list.html',
 				{'page':page,
-				'posts':posts})
+				'posts':posts,
+				'tag':tag})
 
 """ def post_detail(request,year,month,day,post):
 	post = get_object_or_404(Post,slug=post,
